adfb2c63cfc47436244e7ca956fd73224b888e40
Angel <anlure@gmail.com>
2017-08-12 11:54:09 -0400

    configure routes and production for amazon s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     path: ':class/:attachment/:id/:style/:filename',
-    #s3_host_name: 's3-us-east-1.amazonaws.com',
+    s3_host_name: 's3-us-east-1.amazonaws.com',
     s3_credentials: {
       bucket: 'houseclip',
       access_key_id: 'AKIAIX7HRAKYQQ6K6EAA',
