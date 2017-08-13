47740ff568f0bfe9ae73cb3abe137a400142c068
Angel <anlure@gmail.com>
2017-08-12 20:11:20 -0400

    set up env files

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,5 @@
 require_relative 'boot'
 
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,5 +11,13 @@ module Housclip
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
+
   end
 end
