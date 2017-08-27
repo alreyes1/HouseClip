f94dc9e8ab34a5df46d3a4636b967b039bc33ee6
Angel <anlure@gmail.com>
2017-08-27 15:37:51 -0400

    Refactor reviews to separate between host and guest reviews

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,17 +1,10 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [:show, :edit, :update]
+  before_action :set_room, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:show]
+  before_action :is_authorised, only: [:listing, :pricing, :description, :photo_upload, :amenities, :location, :update]
+
   def index
     @rooms = current_user.rooms
-  end
-
-  def show
-    @photos = @room.photos
-
-    @booked = Reservation.where("room_id = ? AND user_id = ?", @room.id, current_user.id).present? if current_user
-
-    @reviews = @room.reviews
-    @hasReview = @reviews.find_by(user_id: current_user.id) if current_user
   end
 
   def new
@@ -20,55 +13,89 @@ class RoomsController < ApplicationController
 
   def create
     @room = current_user.rooms.build(room_params)
-
     if @room.save
-
-      if params[:images]
-        params[:images].each do |image|
-          @room.photos.create(image: image)
-      end
-    end
-
-    @photos = @room.photos
-      redirect_to edit_room_path(@room), notice: "Saved..."
+      redirect_to listing_room_path(@room), notice: "Saved..."
     else
+      flash[:alert] = "Something went wrong..."
       render :new
     end
   end
 
-  def edit
-    if current_user.id == @room.user.id
-      @photos = @room.photos
+  def show
+    @photos = @room.photos
+    @guest_reviews = @room.guest_reviews
+  end
 
-    else
-      redirect_to root_path, notice: "You dont have permisison."
-    end
+  def listing
+  end
+
+  def pricing
+  end
+
+  def description
+  end
+
+  def photo_upload
+    @photos = @room.photos
+  end
+
+  def amenities
+  end
+
+  def location
   end
 
   def update
-    if @room.update(room_params)
 
-      if params[:images]
-        params[:images].each do |image|
-          @room.photos.create(image: image)
-      end
-    end
+    new_params = room_params
+    new_params = room_params.merge(active: true) if is_ready_room
 
-      redirect_to edit_room_path(@room), notice: "Updated..."
-
+    if @room.update(new_params)
+      flash[:notice] = "Saved..."
     else
-      render :edit
+      flash[:alert] = "Something went wrong..."
     end
+    redirect_back(fallback_location: request.referer)
+  end
 
+  # --- Reservations ---
+  def preload
+    today = Date.today
+    reservations = @room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+
+    render json: reservations
+  end
+
+  def preview
+    start_date = Date.parse(params[:start_date])
+    end_date = Date.parse(params[:end_date])
+
+    output = {
+      conflict: is_conflict(start_date, end_date, @room)
+    }
+
+    render json: output
   end
 
   private
-  def set_room
-    @room = Room.find(params[:id])
-  end
-def room_params
-  params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+    def is_conflict(start_date, end_date, room)
+      check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+      check.size > 0? true : false
+    end
 
-end
+    def set_room
+      @room = Room.find(params[:id])
+    end
 
+    def is_authorised
+      redirect_to root_path, alert: "You don't have permission" unless current_user.id == @room.user_id
+    end
+
+    def is_ready_room
+      !@room.active && !@room.price.blank? && !@room.listing_name.blank? && !@room.photos.blank? && !@room.address.blank?
+    end
+
+    def room_params
+      params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+    end
 end
