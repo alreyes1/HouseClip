0db7bc23bcd97e759e6528fb80c17c454d4dfdfc
Angel <anlure@gmail.com>
2017-08-27 16:15:25 -0400

    Refactor reviews to separate between host and guest reviews

--- config/routes.rb
+++ config/routes.rb
@@ -7,33 +7,21 @@ Rails.application.routes.draw do
   						path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
   						controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
-  resources :users, only: [:show]
-  resources :rooms
-    resources :photos, only: [:create, :destroy]
 
+resources :users, only: [:show]
+resources :rooms, except: [:edit] do
+  resources :photos, only: [:create, :destroy]
+  resources :reservations, only: [:create]
+end
 
-  resources :rooms do
-    resources :reservations, only: [:create]
-  end
+resources :guest_reviews, only: [:create, :destroy]
+resources :host_reviews, only: [:create, :destroy]
 
-  resources :conversations, only: [:index, :create] do
-    resources :messages, only: [:index, :create]
-  end
+get '/preload' => 'reservations#preload'
+get '/preview' => 'reservations#preview'
+get '/your_trips' => 'reservations#your_trips'
+get '/your_reservations' => 'reservations#your_reservations'
+post '/notify' => 'reservations#notify'
 
-  resources :rooms do
-    resources :guest_reviews, only: [:create, :destroy]
-    resources :host_reviews, only: [:create, :destroy]
-  end
-
-  get '/preload' => 'reservations#preload'
-  get '/preview' => 'reservations#preview'
-
-  get '/your_trips' => 'reservations#your_trips'
-  get '/your_reservations' => 'reservations#your_reservations'
-
-  post '/notify' => 'reservations#notify'
-  post '/your_trips' => 'reservations#your_trips'
-
-  get '/search' => 'pages#search'
-
+get 'search' => 'pages#search'
 end
