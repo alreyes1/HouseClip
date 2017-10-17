33c9264296e4ca9def40359ac8d7cffb92084b28
Angel <anlure@gmail.com>
2017-10-16 20:22:28 -0400

    Created the instant/request controller and view

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -33,9 +33,19 @@ end
       @reservation.room = room
       @reservation.price = room.price
       @reservation.total = room.price * days
-      @reservation.save
+      #@reservation.save
 
-      flash[:notice] = "Booked Successfully!"
+			if @reservation.save
+				if room.Request?
+					flash[:notice] = "Request sent successfully!"
+				else
+					@reservation.Approved!
+					flash[:notice] = "Reservation created successfully!"
+				end
+			else
+				flash[:alert] = "Cannot make a reservation!"
+			end
+			
     end
     redirect_to room
   end
