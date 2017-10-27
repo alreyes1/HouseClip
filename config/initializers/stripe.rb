57492cde9f8201ac5bbcb03a825140f8ea4a6c28
Angel <anlure@gmail.com>
2017-10-26 21:59:32 -0400

    Created Stripe Payment Form

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_Nru3f8wKWnt6wlgh0cEMWT5s',
-  :secret_key => 'sk_test_ppJC9d9xL21SCX3YYoHtpUI1'
+  :publishable_key => ENV["stripe_publishable_key"],
+  :secret_key => ENV["stripe_secret_key"]
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
