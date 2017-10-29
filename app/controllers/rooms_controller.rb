1387bd010e7517f0df7841451855abadc3e4060f
Angel <anlure@gmail.com>
2017-10-28 20:56:15 -0400

    Fix room controller to redirect to room path instead of listing room path

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -14,7 +14,7 @@ class RoomsController < ApplicationController
   def create
     @room = current_user.rooms.build(room_params)
         if @room.save
-          redirect_to listing_room_path(@room), notice: "Saved..."
+          redirect_to room_path(@room), notice: "Saved..."
         else
           flash[:alert] = "Something went wrong..."
           render :new
