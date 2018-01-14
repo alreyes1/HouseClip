a170ef56cbae1d62cd5c7f7e7212683915bdc1d3
Angel <anlure@gmail.com>
2018-01-14 10:33:37 -0500

    fixing action cable for messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -11,6 +11,6 @@ class Conversation < ApplicationRecord
   }
 
   scope :between, -> (sender_id, recipient_id) {
-    where("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id = ?)", sender_id, recipient_id, recipient_id, sender_id)
+    where("(conversations.sender_id = ? OR conversations.recipient_id = ?) OR conversations.sender_id = ? OR conversations.recipient_id = ?", sender_id, recipient_id, recipient_id, sender_id)
   }
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :conversation
   belongs_to :user
+  belongs_to :conversation
 
   validates_presence_of :content, :conversation_id, :user_id
   after_create_commit :create_notification
