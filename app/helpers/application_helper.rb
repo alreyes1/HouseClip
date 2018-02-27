185f8a2ec2020a90b13128d94c81bf79ee0f04ec
Angel <anlure@gmail.com>
2018-02-26 19:15:02 -0500

    Added default profile pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,15 @@
 module ApplicationHelper
+  
   def avatar_url(user)
     if user.image
       "http://graph.facebook.com/#{user.uid}/picture?type=large"
-    elsif
-      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
     else
-      "/assets/Logo150.png"
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      default_url = "assets/Logo150.png"
+      url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{150}&d=#{CGI::escape(default_url)}"
     end
   end
+
   def stripe_express_path
     "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_BfMAoMggW3Vrhjs12dmECe9KXrml4mCd&scope=read_write"
 
