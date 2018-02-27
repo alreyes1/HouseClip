0dc3764efda6766a199029441f5b37907c8a6af2
Angel <anlure@gmail.com>
2018-02-26 19:23:55 -0500

    Added default profile pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  
+
   def avatar_url(user)
     if user.image
       "http://graph.facebook.com/#{user.uid}/picture?type=large"
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      default_url = "assets/Logo150.png"
-      url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{150}&d=#{CGI::escape(default_url)}"
+      default_url = "https://houseclip.properties/Logo150.png"
+      url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
     end
   end
 
