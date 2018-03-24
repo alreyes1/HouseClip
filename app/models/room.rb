31533c38c9d8735d97badcca1f18d05ae9d7ecf8
Angel <anlure@gmail.com>
2018-03-23 23:17:22 -0400

    adding delete btn to rooms list

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,9 +3,9 @@ class Room < ActiveRecord::Base
 
   belongs_to :user
   has_many :photos, dependent: :destroy
-  has_many :reservations
-  has_many :guest_reviews
-  has_many :calendars
+  has_many :reservations, dependent: :destroy
+  has_many :guest_reviews, dependent: :destroy
+  has_many :calendars, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
