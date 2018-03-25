ce124a974366258c93080ee65fe13566acd34708
Angel <anlure@gmail.com>
2018-03-25 13:34:39 -0400

    update aws-sdk-s3 gem and paperclip

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,12 +12,12 @@ class Photo < ApplicationRecord
       access_key_id: ENV.fetch('access_key_id'),
       secret_access_key: ENV.fetch('secret_access_key'),
       s3_region: ENV.fetch('aws_region'),
-      s3_host_name: ENV.fetch('s3_host_name')
+      path: ':class/:attachment/:id/:style/:filename',
     },
 
       s3_permissions: "private",
-      url: ENV.fetch('s3_host_name')
-      Paperclip::Attachment.default_options[:url] = ENV.fetch('s3_host_name')
+      url: ":s3_domain_url"
+      Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
       Paperclip::Attachment.default_options[:path] = ':class/:attachment/:id/:style/:filename'
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
