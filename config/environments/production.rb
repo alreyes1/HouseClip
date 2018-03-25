6d7075fb72332f2da8857e855a884ddc9f23b0b9
Angel <anlure@gmail.com>
2018-03-25 17:07:48 -0400

    update aws-sdk-s3 gem and paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
     s3_credentials: {
         bucket: ENV['bucket'],
         access_key_id: ENV['access_key_id'],
-        secret_access_key: ENV['secret_access_key'],
-        s3_host_name: 's3.amazonaws.com'
+        secret_access_key: ENV['secret_access_key']
+      #  s3_host_name: 's3.amazonaws.com'
       }
     }
 
