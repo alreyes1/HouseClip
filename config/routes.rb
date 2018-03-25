c338eb34121a35ac0c6faf6e820be99a81b4263c
Angel <anlure@gmail.com>
2018-03-25 19:45:19 -0400

    update aws-sdk-s3 gem and paperclip

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,12 @@ end
 resources :rooms
 resources :rooms, except: [:edit] do
   member do
+    get 'listing'
+    get 'pricing'
+    get 'description'
+    get 'photo_upload'
+    get 'amenities'
+    get 'location'
     get 'preload'
     get 'preview'
   end
